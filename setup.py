e20f2b8a41f16deb9abdb24babdc22686779d7aa
Ross Patterson <me@rpatterson.net>
2012-05-23 12:36:59 -0400

    Next attempt, abuse scritps

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='iisfcgi',
       license='GPL',
       install_requires=['flup'],
       extras_require=dict(config=['PasteDeploy']),
-      package_data={'iisfcgi': ['*.ini']},
+      scripts=['test.ini'],
       entry_points={'console_scripts':
                         ['iisfcgi = iisfcgi:run',
                          'iisfcgi_deploy = iisfcgi:deploy_console'],
