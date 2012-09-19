a0587259b745fe788d64cf53c718671b5bf8c3c2
Ross Patterson <me@rpatterson.net>
2012-09-19 10:58:15 -0700

    Remove some debugging code

--- sample.msdeploy/setup.py
+++ sample.msdeploy/setup.py
@@ -4,26 +4,22 @@ import iisfcgi
 
 version = '0.1'
 
-try:
-    setup(name='IISFCGISampleApp',
-          version=version,
-          description="""Sample app demonstrating the use of IISFCGI \
-    with Microsoft Web Deploy.""",
-          classifiers=[
-            "Environment :: Web Environment",
-            "License :: OSI Approved :: GNU General Public License (GPL)",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-            ],
-          keywords='python IIS FastCGI',
-          author='Ross Patterson',
-          author_email='me@rpatterson.net',
-          url='http://github.com/rpatterson/iisfcgi',
-          license='GPL version 3',
-          cmdclass={'build': iisfcgi.MSDeployBuild,
-                    'sdist': iisfcgi.MSDeploySDist},
-          )
-except:
-    import pdb, sys; pdb.post_mortem(sys.exc_info()[2])
-    raise
+setup(name='IISFCGISampleApp',
+      version=version,
+      description="""Sample app demonstrating the use of IISFCGI \
+with Microsoft Web Deploy.""",
+      classifiers=[
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        ],
+      keywords='python IIS FastCGI',
+      author='Ross Patterson',
+      author_email='me@rpatterson.net',
+      url='http://github.com/rpatterson/iisfcgi',
+      license='GPL version 3',
+      cmdclass={'build': iisfcgi.MSDeployBuild,
+                'sdist': iisfcgi.MSDeploySDist},
+      )
