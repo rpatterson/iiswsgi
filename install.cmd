9b64c08fffdd8a3110e976160037576f907bdc96
Ross Patterson <me@rpatterson.net>
2012-09-20 23:29:26 -0700

    Try moving the fastCGI application registration into web.config to demonstrate that it doesn't work as documented:
    http://www.iis.net/configreference/system.webserver/fastcgi/application#005
    
    If installing the MSDeploy package (as generated by `python
    build_package.py`) with the `web.config` file which includes the
    `<fastCgi><application ...` stanza, it doesn't work.  But it does work
    as soon as we run the `appcmd.exe` command as in `install.cmd`.

--- install.cmd
+++ install.cmd
@@ -1,1 +1,2 @@
-"%IIS_BIN%\appcmd.exe" set config -section:system.webServer/fastCgi /+"[fullPath='%%SystemDrive%%\Python27\python.exe',arguments='-u %%SystemDrive%%\Python27\Scripts\iiswsgi-script.py',maxInstances='1',monitorChangesTo='%%SystemDrive%%\Python27\Scripts\iiswsgi-script.py']" /commit:apphost
+set IIS_BIN=%ProgramFiles%\IIS Express
+"%IIS_BIN%\appcmd.exe" set config -section:system.webServer/fastCgi /+"[fullPath='%SystemDrive%\Python27\python.exe',arguments='-u %SystemDrive%\Python27\Scripts\iiswsgi-script.py',maxInstances='1',monitorChangesTo='%SystemDrive%\Python27\Scripts\iiswsgi-script.py']" /commit:apphost
