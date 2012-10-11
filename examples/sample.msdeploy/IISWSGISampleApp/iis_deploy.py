9077d91a6361965322c4c7db3afee7d04fdb813f
Ross Patterson <me@rpatterson.net>
2012-10-11 08:32:06 -0700

    Try using config file, not working yet

--- examples/sample.msdeploy/IISWSGISampleApp/iis_deploy.py
+++ examples/sample.msdeploy/IISWSGISampleApp/iis_deploy.py
@@ -6,7 +6,8 @@ from iiswsgi import deploy
 def main():
     deploy.install_fcgi_app(
         fullPath="%SystemDrive%\Python27\python.exe",
-        arguments="-u %SystemDrive%\Python27\Scripts\iiswsgi-script.py",
+        arguments=("-u %SystemDrive%\Python27\Scripts\iiswsgi-script.py "
+                   "-c %APPL_PHYSICAL_PATH%\iis_fcgi.ini"),
         monitorChangesTo="%SystemDrive%\Python27\Scripts\iiswsgi-script.py",
         maxInstances="1")
 
