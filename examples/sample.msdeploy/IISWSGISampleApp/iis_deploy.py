452d53fb714edae014d74dcf3dc846d4a1e2f56f
Ross Patterson <me@rpatterson.net>
2012-10-12 05:13:31 -0700

    Read `appcmd.exe` fcgi app attributes from web.config

--- examples/sample.msdeploy/IISWSGISampleApp/iis_deploy.py
+++ examples/sample.msdeploy/IISWSGISampleApp/iis_deploy.py
@@ -4,13 +4,7 @@ from iiswsgi import deploy
 
 
 def main():
-    deploy.install_fcgi_app(
-        fullPath="%SystemDrive%\Python27\python.exe",
-        arguments=('-u %SystemDrive%\Python27\Scripts\iiswsgi-script.py '
-                   '-c "%IIS_SITES_HOME%\%IISEXPRESS_SITENAME%\iis_fcgi.ini"'),
-        # Can't use environment variables in monitorChangesTo
-        monitorChangesTo="C:\Python27\Scripts\iiswsgi-script.py",
-        maxInstances="1")
+    deploy.install_fcgi_app()
 
 if __name__ == '__main__':
     main()
--- iiswsgi/deploy.py
+++ iiswsgi/deploy.py
@@ -4,6 +4,7 @@ import subprocess
 import optparse
 import logging
 
+from xml.dom import minidom
 
 root = logging.getLogger()
 logger = logging.getLogger('iiswsgi.deploy')
@@ -16,7 +17,15 @@ app_attr_defaults_init = dict(
     maxInstances=1)
 
 
+def get_web_config_apps(web_config):
+    doc = minidom.parse(web_config)
+    for fcgi in doc.getElementsByTagName("fastCgi"):
+        for app in doc.getElementsByTagName("application"):
+            yield dict((key, value) for key, value in app.attributes.items())
+
+
 def install_fcgi_app(appcmd_exe=None,
+                     web_config=None,
                      app_attr_defaults=app_attr_defaults_init,
                      **application_attrs):
     """
@@ -24,18 +33,29 @@ def install_fcgi_app(appcmd_exe=None,
 
     Since registering FastCGI applications doesn't work through
     `web.config`, this script will install the FastCGI app globally
-    into IIS.
+    into IIS.  The attributes for the FCGI application may be given in
+    a `web.config` file or through kwargs.
 
-    The kwargs will be used as attributes for the
-    <fastCgi><application> element installed.  See
+    If a `web.config` file is used, the `app_attr_defaults` will not
+    be used but kwargs will still override `web.config` attributes.
+    If a `web_config` argument is not passed, if the `os.getcwd()` or
+    the directory in the `APPL_PHYSICAL_PATH` environment variable
+    contains a `web.config` file, then that file will be used.  If the
+    `web.config` file selected contains
+    `configuration/system.webServer/fastCgi/application` elements,
+    then IIS FCGI applications will be installed for all of them with
+    attributes overridden by kwargs.  If the `web.config` file
+    selected contains no such elements then the `app_attr_defaults`
+    and kwargs will be used as described below.
+
+    Pass `web_config=False` to disable using a `web.config` file and
+    use `app_attr_defaults`.  In that case, kwargs override
+    `app_attr_defaults`.
+
     http://www.iis.net/ConfigReference/system.webServer/fastCgi/application
     for more details on the valid attributes and their affects.
     """
-    # TODO read from web.config instead?
-    app_attrs = app_attr_defaults.copy()
-    app_attrs.update(application_attrs)
-    appcmd_args = ",".join(
-        "{0}='{1}'".format(*item) for item in app_attrs.iteritems())
+    logging.basicConfig(level=logging.INFO)
 
     if appcmd_exe is None:
         appcmd_exe = '{WINDIR}\\System32\\inetsrv\\appcmd.exe'
@@ -45,12 +65,32 @@ def install_fcgi_app(appcmd_exe=None,
             appcmd_exe = '{PROGRAMFILES}\\IIS Express\\appcmd.exe'
     appcmd_exe = appcmd_exe.format(**os.environ)
 
-    appcmd_cmd = (
-        appcmd_exe, "set", "config", "-section:system.webServer/fastCgi",
-        '/+[{0}]'.format(appcmd_args), '/commit:apphost')
-    logger.info('Installing IIS FastCGI application: {0!r}'.format(
-        ' '.join(appcmd_cmd)))
-    subprocess.check_call(appcmd_cmd)
+    if web_config is None:
+        # Search for default web.config
+        if os.path.exists('web.config'):
+            web_config = 'web.config'
+        elif 'APPL_PHYSICAL_PATH' in os.environ:
+            web_config = os.path.join(
+                os.environ['APPL_PHYSICAL_PATH'], 'web.config')
+
+    if web_config:
+        apps = get_web_config_apps(web_config)
+    else:
+        apps = [app_attr_defaults.copy()]
+
+    for app_attrs in apps:
+        # Override with kwargs
+        app_attrs.update(application_attrs)
+        # format attributes for appcmd.exe
+        appcmd_args = ",".join(
+            "{0}='{1}'".format(*item) for item in app_attrs.iteritems())
+
+        appcmd_cmd = (
+            appcmd_exe, "set", "config", "-section:system.webServer/fastCgi",
+            '/+[{0}]'.format(appcmd_args), '/commit:apphost')
+        logger.info('Installing IIS FastCGI application: {0!r}'.format(
+            ' '.join(appcmd_cmd)))
+        subprocess.check_call(appcmd_cmd)
 
 
 def install_fcgi_app_console(args=None):
