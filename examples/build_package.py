5c6a2fd6b9fa538177e1906f59db5050016490b9
Ross Patterson <me@rpatterson.net>
2012-10-12 08:51:39 -0700

    Support multiple msdeploy packages

--- examples/build_package.py
+++ examples/build_package.py
@@ -13,15 +13,59 @@ logger = logging.getLogger('iiswsgi.build_package')
 
 cwd = os.getcwd()
 
-try:
-    os.chdir(os.path.join(os.path.dirname(__file__), 'sample.msdeploy'))
-    subprocess.check_call([sys.executable, 'setup.py', 'sdist'])
-    package_size = os.path.getsize(
-        os.path.join('dist', 'IISWSGISampleApp-0.1.zip'))
-    package_sha1 = subprocess.check_output([
-        'fciv', '-sha1', os.path.join('dist', 'IISWSGISampleApp-0.1.zip')])
-finally:
-    os.chdir(cwd)
+feed_path = os.path.join(os.path.dirname(__file__), 'web-pi.xml')
+doc = minidom.parse(feed_path + '.in')
+
+for name in os.listdir(os.path.dirname(__file__)):
+    if os.path.splitext(name)[1] != '.msdeploy':
+        # not an msdeploy package
+        continue
+
+    try:
+        os.chdir(os.path.join(os.path.dirname(__file__), name))
+        subprocess.check_call([sys.executable, 'setup.py', 'sdist'])
+        os.chdir('dist')
+        latest_package = max(
+            (package for package in os.listdir('.')
+             if os.path.splitext(package)[1] == '.zip'),
+            key=os.path.getmtime)
+        package_size = os.path.getsize(latest_package)
+        package_sha1 = subprocess.check_output([
+            'fciv', '-sha1', latest_package])
+    finally:
+        os.chdir(cwd)
+
+    package_name = latest_package.split('-', 1)[0]
+
+    installer_dir = os.path.join(
+        os.environ['LOCALAPPDATA'], 'Microsoft', 'Web Platform Installer',
+        'installers', package_name)
+    if os.path.exists(installer_dir):
+        logger.info('Removing the cached MSDeploy package: {0}'.format(
+            installer_dir))
+        shutil.rmtree(installer_dir)
+
+    package_size_kb = int(round(package_size / 1024.0))
+    for entry in doc.getElementsByTagName('entry'):
+        productIds = entry.getElementsByTagName("productId")
+        if productIds and productIds[0].firstChild.data == package_name:
+            break
+    else:
+        raise ValueError('Could not find <entry> for {0} in {1}'.format(
+            package_name, feed_path))
+
+    size_elem = entry.getElementsByTagName('fileSize')[0]
+    size_elem.firstChild.data = u'{0}'.format(package_size_kb)
+    logger.info('Set Web Platform Installer <fileSize> to {0}'.format(
+        package_size_kb))
+
+    package_sha1_value = package_sha1.rsplit('\r\n', 2)[-2].split(' ', 1)[0]
+    sha1_elem = entry.getElementsByTagName('sha1')[0]
+    sha1_elem.firstChild.data = u'{0}'.format(package_sha1_value)
+    logger.info('Set Web Platform Installer <sha1> to {0}'.format(
+        package_sha1_value))
+
+doc.writexml(open(feed_path, 'w'))
 
 feed_dir = os.path.join(
     os.environ['LOCALAPPDATA'], 'Microsoft', 'Web Platform Installer')
@@ -37,31 +81,6 @@ for feed in os.listdir(feed_dir):
             'Removing the Web Platform Installer cached feed at {0}'.format(
                 feed_path))
         os.remove(feed_path)
-
-installer_dir = os.path.join(
-    os.environ['LOCALAPPDATA'], 'Microsoft', 'Web Platform Installer',
-    'installers', 'IISWSGISampleApp')
-if os.path.exists(installer_dir):
-    logger.info('Removing the cached MSDeploy package: {0}'.format(
-        installer_dir))
-    shutil.rmtree(installer_dir)
-
-feed_path = os.path.join(os.path.dirname(__file__), 'web-pi.xml')
-doc = minidom.parse(feed_path + '.in')
-
-package_size_kb = int(round(package_size / 1024.0))
-size_elem = doc.getElementsByTagName('fileSize')[0]
-size_elem.firstChild.data = u'{0}'.format(package_size_kb)
-logger.info('Set Web Platform Installer <fileSize> to {0}'.format(
-    package_size_kb))
-
-package_sha1_value = package_sha1.rsplit('\r\n', 2)[-2].split(' ', 1)[0]
-sha1_elem = doc.getElementsByTagName('sha1')[0]
-sha1_elem.firstChild.data = u'{0}'.format(package_sha1_value)
-logger.info('Set Web Platform Installer <sha1> to {0}'.format(
-    package_sha1_value))
-
-doc.writexml(open(feed_path, 'w'))
 
 # Clean up likely stale stamp files
 iis_sites_home = os.path.join(
