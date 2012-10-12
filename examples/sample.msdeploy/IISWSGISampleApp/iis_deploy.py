7534c065009374d20c797d9d0276de5ffd4f00af
Ross Patterson <me@rpatterson.net>
2012-10-12 00:56:21 -0700

    Looks like APPL_PHYSICAL_PATH isn't available at startup

--- examples/sample.msdeploy/IISWSGISampleApp/iis_deploy.py
+++ examples/sample.msdeploy/IISWSGISampleApp/iis_deploy.py
@@ -6,8 +6,8 @@ from iiswsgi import deploy
 def main():
     deploy.install_fcgi_app(
         fullPath="%SystemDrive%\Python27\python.exe",
-        arguments=("-u %SystemDrive%\Python27\Scripts\iiswsgi-script.py "
-                   "-c %APPL_PHYSICAL_PATH%\iis_fcgi.ini"),
+        arguments=('-u %SystemDrive%\Python27\Scripts\iiswsgi-script.py '
+                   '-c "%IIS_SITES_HOME%\%IISEXPRESS_SITENAME%\iis_fcgi.ini"'),
         # Can't use environment variables in monitorChangesTo
         monitorChangesTo="C:\Python27\Scripts\iiswsgi-script.py",
         maxInstances="1")
