377adffe6daef0cd820c3ce0f84b0bb3d7487c7f
Ross Patterson <me@rpatterson.net>
2012-10-13 14:21:21 -0700

    Bring back the project name and install as develop egg, resolves app import error

--- examples/pyramid.msdeploy/PyramidApp/iis_deploy.py
+++ examples/pyramid.msdeploy/PyramidApp/iis_deploy.py
@@ -10,10 +10,22 @@ logger = logging.getLogger('pyramid.iiswsgi')
 
 def main():
     logging.basicConfig(level=logging.INFO)
+
     pcreate = os.path.join(os.path.dirname(sys.executable), 'pcreate.exe')
-    args = [pcreate, '-s', '__pyramid_scaffold__', '.']
+    args = [pcreate, '-s', '__pyramid_scaffold__', '__pyramid_project__']
     logger.info('Creating Pyramid project: {0}'.format(' '.join(args)))
     subprocess.check_call(args)
+
+    cwd = os.getcwd()
+    args = [sys.executable, 'setup.py', 'develop']
+    logger.info(
+        'Installing __pyramid_project__ project for development: {0}'.format(
+            ' '.join(args)))
+    os.chdir('__pyramid_project__')
+    try:
+        subprocess.check_call(args)
+    finally:
+        os.chdir(cwd)
 
 if __name__ == '__main__':
     main()
