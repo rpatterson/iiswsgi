34ee450b38b4cf1cd6a295d35fa5b5c221760328
Ross Patterson <me@rpatterson.net>
2012-10-13 04:35:37 -0700

    Fix verbose option for conversion to argparse

--- iiswsgi/options.py
+++ iiswsgi/options.py
@@ -7,20 +7,22 @@ root = logging.getLogger()
 logger = logging.getLogger('iiswsgi')
 
 
-def verbose_option(value):
-    root.setLevel(root.level - 10)
-    if root.level == logging.DEBUG:
-        # Some useful startup debugging info
-        logger.debug('os.getcwd(): {0}'.format(os.getcwd()))
-        logger.debug('sys.argv: {0}'.format(sys.argv))
-        logger.debug('os.environ:\n{0}'.format(
-            '\n'.join('{0}={1}'.format(key, value)
-                      for key, value in os.environ.iteritems())))
-    return root.level
+class VerboseAction(argparse.Action):
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        root.setLevel(root.level - 10)
+        if root.level == logging.DEBUG:
+            # Some useful startup debugging info
+            logger.debug('os.getcwd(): {0}'.format(os.getcwd()))
+            logger.debug('sys.argv: {0}'.format(sys.argv))
+            logger.debug('os.environ:\n{0}'.format(
+                '\n'.join('{0}={1}'.format(key, value)
+                          for key, value in os.environ.iteritems())))
+        setattr(namespace, self.dest, root.level)
 
 parent_parser = argparse.ArgumentParser(add_help=False)
 verbose = parent_parser.add_argument(
-    "-v", "--verbose", default=root.level, dest='verbose', type=verbose_option,
+    "-v", "--verbose", nargs=0, action=VerboseAction,
     help=("Increase the verbosity of logging.  "
           "Can be given multiple times.  "
           "Pass before other options to ensure that logging includes "
