b3bcd2449ae0239e883dbafed742782a68d1125c
Ross Patterson <me@rpatterson.net>
2012-10-13 05:45:31 -0700

    Generate MSDeployProviderOptions from Manifest.xml.in

--- iiswsgi/setup.py
+++ iiswsgi/setup.py
@@ -1,17 +1,105 @@
 import os
+import subprocess
+import tempfile
+import zipfile
+import shutil
+
+from xml.dom import minidom
 
 from distutils.command import build
 from distutils.command import sdist
 from distutils import archive_util
+from distutils import log
+from distutils import errors
+
+# TODO upload
 
 
 class MSDeployBuild(build.build):
     """Build an MSDeploy zip package for installation into IIS."""
 
+    manifest_name = 'Manifest.xml'
+    msdeploy = os.path.join(
+        os.environ['PROGRAMFILES'], 'IIS', 'Microsoft Web Deploy V3',
+        'msdeploy.exe')
+    dest_name = 'runCommand.zip'
+
     def initialize_options(self):
         """Be more discriminating about what to prune."""
         build.build.initialize_options(self)
         self.build_base = 'build/'
+
+    def run(self):
+        # TODO use sub_commands
+        result = build.build.run(self)
+
+        manifest_template = self.manifest_name + '.in'
+        if not os.path.exists(manifest_template):
+            log.warn('No Web Deploy manifest template found at {0}'.format(
+                manifest_template))
+            # No manifest template, don't update real manifest
+            return result
+
+        manifest = minidom.parse(manifest_template)
+        for runcommand in manifest.getElementsByTagName('runCommand'):
+            # Collect the attributes that need to be passed as settings
+            path = None
+            settings_attrs = {}
+            for name, value in runcommand.attributes.items():
+                if name == 'path':
+                    # provider value/key attribute
+                    path = value
+                    continue
+                elif name.startswith('MSDeploy.'):
+                    # Leave these alone
+                    continue
+                settings_attrs[name] = value
+                # Remove them from the output XML
+                runcommand.removeAttribute(name)
+            if path is None:
+                raise errors.DistutilsFileError(
+                    'No `path` attribute in a <runCommand> element in {0}'
+                    .format(manifest_template))
+
+            # Assemble the msdeploy.exe source command line
+            settings = ','.join('{0}={1}'.format(*item) for item in
+                                settings_attrs.items())
+            source = '-source:runCommand="{0}"{1}'.format(path, settings)
+
+            tmp = tempfile.mkdtemp()
+            package = os.path.join(tmp, 'runCommand.zip')
+            try:
+                args = (
+                    '"{msdeploy}" -verb:sync {source} -dest:package={package}'
+                    .format(msdeploy=self.msdeploy, source=source,
+                            package=package))
+                log.info('Generating runCommand manifest: {0}'.format(args))
+                subprocess.check_call(args, shell=True)
+                tmp_manifest = minidom.parseString(
+                    zipfile.ZipFile(package).read('archive.xml'))
+            finally:
+                shutil.rmtree(tmp)
+
+            new_runcommands = tmp_manifest.getElementsByTagName('runCommand')
+            if not new_runcommands:
+                raise errors.DistutilsExecError(
+                    'No <runCommand> elements found in {0}:archive.xml'
+                    .format(package))
+            elif len(new_runcommands) > 1:
+                raise errors.DistutilsExecError(
+                    'Multiple <runCommand> elements found in {0}:archive.xml'
+                    .format(package))
+
+            options = new_runcommands[0].getAttribute(
+                'MSDeploy.MSDeployProviderOptions')
+            runcommand.setAttribute(
+                'MSDeploy.MSDeployProviderOptions', options)
+
+        log.info('Writing Web Deploy manifest to {0}'.format(
+            self.manifest_name))
+        manifest.writexml(open(self.manifest_name, 'w'))
+
+        return result
 
 
 def make_zipfile(base_name, base_dir, verbose=0, dry_run=0):
