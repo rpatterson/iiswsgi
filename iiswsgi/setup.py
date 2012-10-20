299c8a54339dc0601da698eec062d1c4275c00ed
Ross Patterson <me@rpatterson.net>
2012-10-20 13:00:32 -0700

    Shorten paths to avoid windows issues.
    Do away with the need for two levels in the package by manually
    putting Manifest.xml and Parameters.xml outside the directoryn in the
    zip archive.  Also shorten the build tree name to just the version
    number.

--- iiswsgi/setup.py
+++ iiswsgi/setup.py
@@ -140,48 +140,71 @@ class MSDeployBuild(build.build):
         return app_name
 
 
-def make_zipfile(base_name, base_dir, verbose=0, dry_run=0):
-    """Create a zip file from all the files under 'base_dir'.
-
-    The output zip file will be named 'base_name' + ".zip".  Uses either the
-    "zipfile" Python module (if available) or the InfoZIP "zip" utility
-    (if installed and found on the default search path).  If neither tool is
-    available, raises DistutilsExecError.  Returns the name of the output zip
-    file.
-    """
-    import zipfile
-
-    zip_filename = base_name + ".zip"
-    base_len = len(base_dir + os.sep)
-    archive_util.mkpath(os.path.dirname(zip_filename), dry_run=dry_run)
-
-    archive_util.log.info("creating '%s' and adding '%s' to it",
-             zip_filename, base_dir)
-
-    if not dry_run:
-        zip = zipfile.ZipFile(zip_filename, "w",
-                              compression=zipfile.ZIP_DEFLATED)
-
-        for dirpath, dirnames, filenames in os.walk(base_dir):
-            for name in filenames:
-                src_path = os.path.normpath(os.path.join(dirpath, name))
-                dst_path = src_path[base_len:]
-                if os.path.isfile(src_path):
-                    zip.write(src_path, dst_path)
-                    archive_util.log.info("adding '%s'" % dst_path)
-        zip.close()
-
-    return zip_filename
-
-
 class MSDeploySDist(sdist.sdist):
     """Create an MSDeploy zip package for installation into IIS."""
 
+    msdeploy_files = ('Manifest.xml', 'Parameters.xml')
+
+    def make_distribution(self):
+        """Minimize path lenght to avoid windows issues."""
+        # Copied from distutils.command.sdist.sdist.make_distribution
+
+        # Don't warn about missing meta-data here -- should be (and is!)
+        # done elsewhere.
+        base_dir = self.distribution.get_version()
+        base_name = os.path.join(
+            self.dist_dir, self.distribution.get_fullname())
+
+        self.make_release_tree(base_dir, self.filelist.files)
+        archive_files = []              # remember names of files we create
+        # tar archive must be created last to avoid overwrite and remove
+        if 'tar' in self.formats:
+            self.formats.append(self.formats.pop(self.formats.index('tar')))
+
+        for fmt in self.formats:
+            file = self.make_archive(base_name, fmt, base_dir=base_dir,
+                                     owner=self.owner, group=self.group)
+            archive_files.append(file)
+            self.distribution.dist_files.append(('sdist', '', file))
+
+        self.archive_files = archive_files
+
+        if not self.keep_temp:
+            sdist.dir_util.remove_tree(base_dir, dry_run=self.dry_run)
+
     def make_archive(self, base_name, format, root_dir=None, base_dir=None,
                      owner=None, group=None):
-        """Create a zip file without a top-level directory."""
-        make_zipfile(base_name, base_dir,
-                     verbose=self.verbose, dry_run=self.dry_run)
+        """Don't inlcude the version number for MSDeploy packages."""
+        dry_run = self.dry_run
+        dist_name = self.distribution.get_name()
+
+        # Copied from distutils.command.sdist.sdist.make_archive
+        zip_filename = base_name + ".zip"
+        base_len = len(base_dir)
+        archive_util.mkpath(os.path.dirname(zip_filename), dry_run=dry_run)
+
+        archive_util.log.info("creating '%s' and adding '%s' to it",
+                 zip_filename, base_dir)
+
+        if not dry_run:
+            zip = zipfile.ZipFile(zip_filename, "w",
+                                  compression=zipfile.ZIP_DEFLATED)
+
+            for filename in self.msdeploy_files:
+                archive_util.log.info("adding '%s'" % filename)
+                zip.write(filename, filename)
+
+            for dirpath, dirnames, filenames in os.walk(base_dir):
+                for name in filenames:
+                    src_path = os.path.normpath(os.path.join(dirpath, name))
+                    dst_path = dist_name + src_path[base_len:]
+                    if os.path.isfile(src_path):
+                        zip.write(src_path, dst_path)
+                        archive_util.log.info("adding '%s'" % dst_path)
+            zip.close()
+
+        return zip_filename
+
 
 # TODO separate actions, msdeploy_package depending on msdeploy_build
 cmdclass = dict(build=MSDeployBuild,
