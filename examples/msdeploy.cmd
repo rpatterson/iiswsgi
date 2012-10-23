a300ec52f530f8e777ec18e983653689bf5eeba0
Ross Patterson <me@rpatterson.net>
2012-10-22 20:09:45 -0700

    Long since unused

--- examples/msdeploy.cmd
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\IIS\Microsoft Web Deploy\msdeploy.exe" -verb:sync -source:package="sample.msdeploy\dist\IISWSGISampleApp-0.1.zip" -dest:auto -setParam:name=ApplicationPath,value="Default Web Site/IISWSGISampleApp" -debug -verbose > sample.msdeploy\dist\MsDeployOutput.txt 2>&1
--- install.cmd
+++
@@ -1,2 +0,0 @@
-set IIS_BIN=%ProgramFiles%\IIS Express
-"%IIS_BIN%\appcmd.exe" set config -section:system.webServer/fastCgi /+"[fullPath='%SystemDrive%\Python27\python.exe',arguments='-u %SystemDrive%\Python27\Scripts\iiswsgi-script.py',maxInstances='1',monitorChangesTo='C:\Python27\Scripts\iiswsgi-script.py']" /commit:apphost
