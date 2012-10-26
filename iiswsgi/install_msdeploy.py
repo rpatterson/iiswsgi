bbf04eca346da9abf24392c6abe416a7b8aeb800
Ross Patterson <me@rpatterson.net>
2012-10-26 01:44:37 -0700

    Windows can't invoke scripts from Python

--- iiswsgi/install_msdeploy.py
+++ iiswsgi/install_msdeploy.py
@@ -184,7 +184,7 @@ class Installer(object):
                 executable = self.setup_virtualenv(bootstrap=bootstrap)
 
                 # Install iiswsgi for the setup commands
-                cmd = [executable, os.path.join(sysconfig.get_path(
+                cmd = [os.path.join(sysconfig.get_path(
                     'scripts', vars=dict(base=os.curdir)),
                     'easy_install' + sysconfig.get_config_var('EXE')),
                        '--find-links', distutils.sysconfig.get_python_lib(),
